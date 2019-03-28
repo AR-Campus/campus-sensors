19d5869f3fd7f946e0a93b248ccf0bc2c45864ba
Julian Mielke <julianmielke@JuliansMBPR2016.dbt.db.de>
2019-03-28 11:54:36 +0100

    server.go File added

---
+++ server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var data []string
+
+type SensorInfo struct {
+	Message string
+}
+
+func Store(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Store data", r.Body)
+	var sensorInfo SensorInfo
+	_ = json.NewDecoder(r.Body).Decode(&sensorInfo)
+	data = append(data, sensorInfo.Message)
+}
+
+func Infos(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get data")
+	json.NewEncoder(w).Encode(data)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(false)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/store", Store)
+	router.HandleFunc("/infos", Infos)
+
+	log.Fatal(http.ListenAndServe(":5555", router))
+
+}
