c03a5a832f15c04f78211a5ecae763c3e95c094d
Julian Mielke <julian.mielke@db-training.de>
2019-04-05 16:39:13 +0200

    main.go extendent -EnvVar Added -FireFly API Get -SensorInfo Json

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"html"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -12,10 +14,17 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var data []string
+var data []SensorInfo
+
+var authKey = os.Getenv("FIREFLY_APIKEY")
 
 type SensorInfo struct {
-	Message string // Hier Struktur von FireFly json
+	device_eui string
+	payload    string
+	parsed     []struct {
+		data map[string]string
+	}
+	// Hier Struktur von FireFly json
 }
 
 func Store(w http.ResponseWriter, r *http.Request) {
@@ -25,9 +34,9 @@ func Store(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Got error")
 	}
 
-	//	var sensorInfo SensorInfo
-	//_ = json.NewDecoder(r.Body).Decode(&sensorInfo)
-	//data = append(data, sensorInfo.Message)
+	var sensorInfo SensorInfo
+	_ = json.NewDecoder(r.Body).Decode(&sensorInfo)
+	data = append(data, sensorInfo)
 }
 
 func Infos(w http.ResponseWriter, r *http.Request) {
@@ -39,11 +48,30 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
+func Sensors(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Sensordaten und Graphische Darstellungen, %q", html.EscapeString(r.URL.Path))
+}
+
 func main() {
+
+	// Fetch last Sensor-Package
+	FireFlyCall := fmt.Sprintf("https://api.fireflyiot.com/api/v1/packets?auth=%v&limit_to_last=10", authKey)
+	response, err := http.Get(FireFlyCall)
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error %s\n", err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+	}
+	jsonData := map[string]string{"firstname": "Nic", "lastname": "Raboy"}
+	jsonValue, _ := json.Marshal(jsonData)
+	response, err = http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(jsonValue))
+
 	router := mux.NewRouter().StrictSlash(false)
 	router.HandleFunc("/", Index)
 	router.HandleFunc("/store", Store)
 	router.HandleFunc("/infos", Infos)
+	router.HandleFunc("/sensors", Sensors)
 
 	herokuPort := os.Getenv("PORT")
 	var port string
