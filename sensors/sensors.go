9340d35ded37dc9ec3fbae38ea2e92207509df12
Julian Mielke <julian.mielke@db-training.de>
2019-05-07 14:44:23 +0200

    add Convert Type from []SensorInfo to []SensorData

--- sensors/sensors.go
+++ sensors/sensors.go
@@ -3,12 +3,19 @@ package sensors
 import (
 	"encoding/json"
 	"log"
+	"time"
 )
 
 type SensorInfo struct {
 	Device_EUI string                   `json:"device_eui"`
 	GWRX       []map[string]interface{} `json:"gwrx"`
 	Parsed     map[string]interface{}   `json:"parsed"`
+}
+
+type SensorData struct {
+	DeviceID     string                 `json:"device_id"`
+	Time         time.Time              `json:"time"`
+	SensorValues map[string]interface{} `json:"sensor_values"`
 }
 
 type FireFlyPackage struct {
@@ -19,32 +26,33 @@ type FireFlySingle struct {
 	Packet SensorInfo `json:"up_packet"`
 }
 
-func ConvertInfos(rawFireFlyData string) []SensorInfo {
+func ConvertSensorType(sensorInfoTypeArray []SensorInfo) []SensorData {
+	result := make([]SensorData, len(sensorInfoTypeArray))
+	for i, entry := range sensorInfoTypeArray {
+		time, _ := time.Parse(time.RFC3339, entry.GWRX[0]["time"].(string))
+		result[i] = SensorData{DeviceID: entry.Device_EUI, Time: time, SensorValues: entry.Parsed}
+	}
+	return result
+}
+
+func ConvertInfos(rawFireFlyData string) []SensorData {
 	log.Print("Raw data", rawFireFlyData)
 	var sensorJson FireFlyPackage
 	err := json.Unmarshal([]byte(rawFireFlyData), &sensorJson)
 	if err != nil {
 		log.Println("Json Unmarshal failed:", err)
-		return make([]SensorInfo, 0)
+		return make([]SensorData, 0)
 	}
-	var result []SensorInfo
-	for _, entry := range sensorJson.Packets {
-		result = append(result, entry)
-	}
-	log.Print("Result", result)
-	return result
+	return ConvertSensorType(sensorJson.Packets)
 }
 
-func ConvertSingle(rawFireFlyData string) []SensorInfo {
+func ConvertSingle(rawFireFlyData string) []SensorData {
 	log.Print("Raw data", rawFireFlyData)
 	var sensorJson FireFlySingle
 	err := json.Unmarshal([]byte(rawFireFlyData), &sensorJson)
 	if err != nil {
 		log.Println("Json Unmarshal failed:", err)
-		return make([]SensorInfo, 0)
+		return make([]SensorData, 0)
 	}
-	var result []SensorInfo
-	result = append(result, sensorJson.Packet)
-	log.Print("Result", result)
-	return result
+	return ConvertSensorType([]SensorInfo{sensorJson.Packet})
 }
